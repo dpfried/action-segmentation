4472113dfb39ce712683d78791efb06bc20b7852
Daniel Fried <dfried@berkeley.edu>
2019-10-05 22:29:28 -0700

    semimarkov test in progress (learns weird means)

--- src/models/semimarkov.py
+++ src/models/semimarkov.py
@@ -2,6 +2,14 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 
+from torch.distributions import MultivariateNormal, Poisson
+
+from models.model import Model, make_optimizer, make_data_loader
+
+from data.corpus import Datasplit
+
+BIG_NEG = -1e9
+
 def sliding_sum(inputs, k):
     # inputs: b x T x c
     # sums sliding windows along the T dim, of length k
@@ -17,10 +25,13 @@ def sliding_sum(inputs, k):
     assert ret.shape == inputs.shape
     return ret
 
-class SemimarkovModel(nn.Module):
+class SemiMarkovModule(nn.Module):
     def __init__(self, n_classes, n_dims, max_k=None):
-        poisson_rates = torch.zeros(n_classes).float()
-        self.poisson_rates = nn.Parameter(poisson_rates, requires_grad=True)
+        super(SemiMarkovModule, self).__init__()
+        self.n_classes = n_classes
+        self.n_dims = n_dims
+        poisson_log_rates = torch.zeros(n_classes).float()
+        self.poisson_log_rates = nn.Parameter(poisson_log_rates, requires_grad=True)
 
         gaussian_means = torch.zeros(n_classes, n_dims).float()
         self.gaussian_means = nn.Parameter(gaussian_means, requires_grad=True)
@@ -33,26 +44,74 @@ class SemimarkovModel(nn.Module):
         transition_logits = torch.zeros(n_classes, n_classes).float()
         self.transition_logits = nn.Parameter(transition_logits, requires_grad=True)
 
+        init_logits = torch.zeros(n_classes).float()
+        self.init_logits = nn.Parameter(init_logits, requires_grad=True)
+        torch.nn.init.uniform_(self.init_logits, 0, 1)
+
         self.max_k = max_k
 
-    def init_gaussian(self, data):
+    def initialize_gaussian(self, data, lengths):
         # batch_size x n_dim
-        assert data.size(1) == self.n_dims
-        mean = torch.mean(data, dim=0)
-        self.gaussian_means.fill_(mean.unsqueeze(0).expand((self.n_classes, self.n_dims)))
+        batch_size, N, n_dim = data.size()
+        assert lengths.size(0) == batch_size
+        assert n_dim == self.n_dims
+        feats = []
+        for i in range(batch_size):
+            feats.append(data[i,:lengths[i]])
+        feats = torch.cat(feats, dim=0)
+        assert feats.dim() == 2
+        assert feats.size(1) == n_dim
+        mean = feats.mean(dim=0, keepdim=True)
+        self.gaussian_means.data = mean.expand((self.n_classes, self.n_dims))
+
+        # TODO: consider using the biased estimator, with torch >= 1.2?
+        self.gaussian_cov.data = torch.diag(feats.var(dim=0))
 
     def initial_log_probs(self):
-        return F.log_softmax(self.init_state_logits)
+        return F.log_softmax(self.init_logits)
 
     def transition_log_probs(self):
-        masked = self.transition_logits.masked_fill(torch.eye(self.n_classes, device=self.transition_logits.device), -float("inf"))
-        return F.log_softmax(masked, dim=1)
+        masked = self.transition_logits.masked_fill(torch.eye(self.n_classes, device=self.transition_logits.device).byte(), BIG_NEG)
+        # transition_logits are indexed: to_state, from_state
+        # so each row should be normalized (in log-space)
+        return F.log_softmax(masked, dim=0)
 
-    def transition_probabilities(self):
-        pass
+    def emission_log_probs(self, features):
+        b, N, d = features.size()
+        feats_reshaped = features.reshape(-1, d)
+        dists = [
+            MultivariateNormal(loc=mean, covariance_matrix=self.gaussian_cov)
+            for mean in self.gaussian_means
+        ]
+        log_probs = [
+            dist.log_prob(feats_reshaped).reshape(b, N, 1) # b x
+            for dist in dists
+        ]
+        return torch.cat(log_probs, dim=2)
+
+    def length_log_probs(self):
+        max_length = self.max_k
+        time_steps = torch.arange(max_length).unsqueeze(-1).expand(max_length, self.n_classes).float()
+        poissons = Poisson(torch.exp(self.poisson_log_rates))
+        return poissons.log_prob(time_steps)
 
     @staticmethod
-    def log_hsmm(transition, emission_scores, init, length_scores, add_eos=False):
+    def labels_to_spans(position_labels):
+        # position_labels: b x N, LongTensor
+        # lengths: b, LongTensor
+        assert not (position_labels == -1).any(), "position_labels already appear span encoded (have -1)"
+        b, N = position_labels.size()
+        last = position_labels[:,0]
+        values = [last.unsqueeze(1)]
+        for n in range(1, N):
+            this = position_labels[:,n]
+            encoded = torch.where(last == this, torch.LongTensor([-1]), this)
+            values.append(encoded.unsqueeze(1))
+            last = this
+        return torch.cat(values, dim=1)
+
+    @staticmethod
+    def log_hsmm(transition, emission_scores, init, length_scores, add_eos):
         """
         Convert HSMM to a linear chain.
         Parameters:
@@ -65,6 +124,7 @@ class SemimarkovModel(nn.Module):
         """
         batch, N_1, C_1 = emission_scores.shape
         K, _C = length_scores.shape
+        assert N_1 >= K
         assert C_1 == _C
         # need to add EOS token
         if add_eos:
@@ -75,20 +135,20 @@ class SemimarkovModel(nn.Module):
             C = C_1
         b = emission_scores.size(0)
         if add_eos:
-            transition_augmented = torch.full((C, C), -float("inf"), device=transition.device)
+            transition_augmented = torch.full((C, C), BIG_NEG, device=transition.device)
             transition_augmented[:C_1, :C_1] = transition
             # can transition from anything to EOS
             transition_augmented[C_1,:] = 0
 
-            init_augmented = torch.full((C,), -float("inf"), device=init.device)
+            init_augmented = torch.full((C,), BIG_NEG, device=init.device)
             init_augmented[:C_1] = init
 
-            length_augmented = torch.full((K, C), -float("inf"), device=length_scores.device)
+            length_augmented = torch.full((K, C), BIG_NEG, device=length_scores.device)
             length_augmented[:,:C_1] = length_scores
             # EOS must be length 1, although I don't think this is checked in the dp
             length_augmented[1,C_1] = 0
 
-            emission_augmented = torch.full((b, N, C), -float("inf"), device=emission_scores.device)
+            emission_augmented = torch.full((b, N, C), BIG_NEG, device=emission_scores.device)
             emission_augmented[:,:N_1,:C_1] = emission_scores
             emission_augmented[:,N_1,C_1] = 0
 
@@ -119,11 +179,38 @@ class SemimarkovModel(nn.Module):
         #             scores[:,start:end,k,:,:] += emission_augmented[:,n,:].view(b,1,1,1,1,C)
         return scores
 
-    def score(self, observations, lengths):
-        batch_size, max_length = observations.size()
+    def add_eos(self, spans, lengths):
+        b, N = spans.size()
+        augmented = torch.cat([spans, torch.full((b,), -1).long().unsqueeze(-1)], dim=1)
+        # assert (augmented[torch.arange(b), lengths] == -1).all()
+        augmented[torch.arange(b),lengths] = self.n_classes
+        return augmented
 
-        # b x N x K x (C+1) x C
-        # batch x time x length x to x from
-        scores = torch.zeros(batch_size, max_length+1, self.max_k, self.n_classes+1, self.n_classes)
+    def score_features(self, features, add_eos):
+        return self.log_hsmm(
+            self.transition_log_probs(),
+            self.emission_log_probs(features),
+            self.initial_log_probs(),
+            self.length_log_probs(),
+            add_eos=add_eos,
+        )
 
-        # initial state distributions
+class SemimarkovModel(Model):
+    @classmethod
+    def add_args(cls, parser):
+        parser.add_argument('--gm_covariance', choices=['full', 'diag', 'tied', 'tied_diag'], default='tied_diag')
+
+    @classmethod
+    def from_args(cls, args, train_data):
+        n_classes = train_data._corpus.n_classes
+        feature_dim = train_data.feature_dim
+        return SemimarkovModel(args, n_classes, feature_dim)
+
+    def __init__(self, args, n_classes, feature_dim):
+        self.args = args
+        self.n_classes = n_classes
+        self.feature_dim = feature_dim
+        self.model = None
+
+    def fit(self, train_data: Datasplit, use_labels=False, callback_fn=None):
+        pass
--- src/models/test_semimarkov.py
+++ src/models/test_semimarkov.py
@@ -1,58 +1,195 @@
-from torch_struct import SemiMarkov, MaxSemiring
-import torch
-import torch.nn.functional as F
+import random
+
 import numpy as np
-from semimarkov import SemimarkovModel
-import itertools
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+import tqdm
+from torch_struct import SemiMarkov, MaxSemiring
 
-#device = torch.device("cuda")
+from torch_struct import SemiMarkovCRF
+
+from models.semimarkov import SemiMarkovModule
+
+# device = torch.device("cuda")
 device = torch.device("cpu")
-
-add_eos = False
 
 sm_max = SemiMarkov(MaxSemiring)
 
-b = 100
-C = 7
-N = 300
-K = 50
-step_length = 20
-
-# b = 2
-# C = 3
-# N = 10
-# K = 5
-# step_length = 2
-
-add_eos = True
+BIG_NEG = -1e9
 
 
-num_steps = N // step_length
-assert N % step_length == 0 # since we're fixing lengths, need to end perfectly
+class ToyDataset(Dataset):
+    def __init__(self, labels, features, lengths):
+        self.labels = labels
+        self.features = features
+        self.lengths = lengths
 
-# trans_scores = torch.from_numpy(np.array([[0,1,0],[0,0,1],[1,0,0]]).T).float().log()
-trans_scores = torch.zeros(C, C, device=device)
-init_scores = torch.full((C,), -float("inf"), device=device)
-init_scores[0] = 0
+    def __len__(self):
+        return self.labels.size(0)
 
-emission_scores = torch.full((b, N, C), -float("inf"), device=device)
+    def __getitem__(self, index):
+        labels = self.labels[index]
+        spans = SemiMarkovModule.labels_to_spans(labels.unsqueeze(0)).squeeze(0)
+        return {
+            'labels': self.labels[index],
+            'features': self.features[index],
+            'lengths': self.lengths[index],
+            'spans': spans,
+        }
 
-for n in range(N):
-    c = (n // step_length) % C
-    emission_scores[:,n,c] = 1
 
-length_scores = torch.full((K, C), -float("inf"), device=device)
-length_scores[step_length,:] = 0
+def synthetic_data(num_data_points=200, C=3, N=100, K=5):
+    def make_synthetic_features(class_labels, shift_constant=5.0):
+        _batch_size, _N = class_labels.size()
+        f = torch.randn((_batch_size, _N, C))
+        shift = torch.zeros_like(f)
+        shift.scatter_(2, class_labels.unsqueeze(2), shift_constant)
+        return shift + f
 
-scores = SemimarkovModel.log_hsmm(trans_scores, emission_scores, init_scores, length_scores, add_eos=add_eos)
-marginals = sm_max.marginals(scores)
+    labels_l = []
+    lengths = []
+    for i in range(num_data_points):
+        if i == 0:
+            length = N
+        else:
+            length = random.randint(K, N)
+        lengths.append(length)
+        lab = []
+        current_step = 0
+        while len(lab) < N:
+            step_length = random.randint(1, K-1)
+            lab.extend([current_step % C] * step_length)
+            current_step += 1
+        lab = lab[:N]
+        labels_l.append(lab)
+    labels = torch.LongTensor(labels_l)
+    features = make_synthetic_features(labels)
+    lengths = torch.LongTensor(lengths)
 
-sequence, extra = sm_max.from_parts(marginals)
+    return labels, features, lengths
 
-for step in range(num_steps):
-    c = step % C
-    assert torch.allclose(sequence[:,step_length*step], torch.full((1,), c).long())
 
-# C == EOS
-if add_eos:
-    assert torch.allclose(sequence[:,-1], torch.full((1,), C).long())
+def partition_rows(arr, N):
+    if isinstance(arr, list):
+        assert N < len(list)
+    else:
+        assert N < arr.size(0)
+    return arr[:N], arr[N:]
+
+
+def test_learn_synthetic():
+    C = 3
+    K = 5
+    N = 10
+    N_train = 150
+    N_test = 50
+
+    epochs = 20
+
+    train_data = ToyDataset(*synthetic_data(num_data_points=N_train, C=C, N=N, K=K))
+    train_loader = DataLoader(train_data, batch_size=1)
+    test_data = ToyDataset(*synthetic_data(num_data_points=N_test, C=C, N=N, K=K))
+    test_loader = DataLoader(train_data, batch_size=1)
+
+    model = SemiMarkovModule(C, C, max_k=K)
+    # model.initialize_gaussian(train_data.features, train_data.lengths)
+    print(model.gaussian_means)
+
+    model.train()
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+
+    for epoch in range(epochs):
+        losses = []
+        for batch in train_loader:
+            # if self.args.cuda:
+            #     features = features.cuda()
+            #     task_indices = task_indices.cuda()
+            #     gt_single = gt_single.cuda()
+            features = batch['features']
+            lengths = batch['lengths']
+            spans = batch['spans']
+            this_N = lengths.max().item()
+            features = features[:,:this_N,:]
+            spans = spans[:,:this_N]
+            scores = model.score_features(features, add_eos=True)
+            dist = SemiMarkovCRF(scores, lengths=lengths+1)
+            true_parts = SemiMarkovCRF.struct.to_parts(model.add_eos(spans, lengths), (C+1, K), lengths=lengths+1).type_as(scores)
+            this_loss = -dist.log_prob(true_parts).mean()
+            this_loss.backward()
+
+            losses.append(this_loss.item())
+
+            optimizer.step()
+            model.zero_grad()
+        print("epoch {} avg loss: {:.4f}".format(epoch, np.mean(losses)))
+
+    return model
+
+
+def test_labels_to_spans():
+    position_labels = torch.LongTensor([[0, 1, 1, 2, 2, 2], [0, 1, 2, 3, 3, 4]])
+    spans = torch.LongTensor([[0, 1, -1, 2, -1, -1], [0, 1, 2, 3, -1, 4]])
+    assert (SemiMarkovModule.labels_to_spans(position_labels) == spans).all()
+
+
+def test_log_hsmm():
+    # b = 100
+    # C = 7
+    # N = 300
+    # K = 50
+    # step_length = 20
+
+    b = 2
+    C = 3
+    N = 10
+    K = 5
+    step_length = 2
+
+    add_eos = True
+
+    padded_length = N + step_length * 2
+
+    lengths_unpadded = torch.full((b,), N).long()
+    lengths_unpadded[0] = padded_length
+    lengths = lengths_unpadded + 1
+
+    num_steps = N // step_length
+    assert N % step_length == 0  # since we're fixing lengths, need to end perfectly
+
+    # trans_scores = torch.from_numpy(np.array([[0,1,0],[0,0,1],[1,0,0]]).T).float().log()
+    trans_scores = torch.zeros(C, C, device=device)
+    init_scores = torch.full((C,), BIG_NEG, device=device)
+    init_scores[0] = 0
+
+    emission_scores = torch.full((b, padded_length, C), BIG_NEG, device=device)
+
+    for n in range(padded_length):
+        c = (n // step_length) % C
+        emission_scores[:, n, c] = 1
+
+    length_scores = torch.full((K, C), BIG_NEG, device=device)
+    length_scores[step_length, :] = 0
+
+    scores = SemiMarkovModule.log_hsmm(trans_scores, emission_scores, init_scores, length_scores, add_eos=add_eos)
+    marginals = sm_max.marginals(scores, lengths=lengths)
+
+    sequence, extra = sm_max.from_parts(marginals)
+
+    for step in range(num_steps):
+        c = step % C
+        assert torch.allclose(sequence[:, step_length * step], torch.full((1,), c).long())
+
+    # C == EOS
+    if add_eos:
+        batch_indices = torch.arange(0, b)
+        assert torch.allclose(sequence[batch_indices, lengths - 1], torch.full((1,), C).long())
+
+
+test_labels_to_spans()
+print("test_labels_to_spans passed")
+
+test_log_hsmm()
+print("test_log_hsmm passed")
+
+model = test_learn_synthetic()
