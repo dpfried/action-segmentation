separate embeddings, LR plateau, fix shuffling, and feature scaling

src/data/corpus.py
@@ -89,6 +89,10 @@ class Video(object):
     def load_features(self):
         raise NotImplementedError("should be implemented by subclasses")
 
+    @property
+    def has_label(self):
+        return self._has_label
+
     def features(self):
         self._check_truncation()
         if self._cache_features:
@@ -111,13 +115,16 @@ class Video(object):
             self._process_features(self.load_features())
             n_frames = self.n_frames()
         assert n_frames is not None
-        if not self._updated_length and (len(self._gt_with_background) != n_frames or not self._features_contain_background):
+        if not self._updated_length and (
+                len(self._gt_with_background) != n_frames or not self._features_contain_background):
             self._updated_length = True
             if WARN_ON_MISMATCH:
                 print(self.name, '# of gt and # of frames does not match %d / %d' %
                       (len(self._gt_with_background), n_frames))
 
-            assert len(self._gt_with_background) - n_frames <= FEATURE_LABEL_MISMATCH_TOLERANCE, "len(self._gt_with_background) = {}, n_frames = {}".format(len(self._gt_with_background), n_frames)
+            assert len(
+                self._gt_with_background) - n_frames <= FEATURE_LABEL_MISMATCH_TOLERANCE, "len(self._gt_with_background) = {}, n_frames = {}".format(
+                len(self._gt_with_background), n_frames)
             min_n = min(len(self._gt_with_background), n_frames)
             # self._gt = self._gt[:min_n]
             # self._gt_with_background = self._gt_with_background[:min_n]
@@ -223,7 +230,7 @@ class Video(object):
 
 
 class Datasplit(Dataset):
-    def __init__(self, corpus, remove_background, full=True, subsample=1):
+    def __init__(self, corpus, remove_background, full=True, subsample=1, feature_downscale=1.0):
         self._corpus = corpus
         self._remove_background = remove_background
         self._full = full
@@ -260,6 +267,8 @@ class Datasplit(Dataset):
 
         self.subsample = subsample
 
+        self.feature_downscale = feature_downscale
+
         # logger.debug('min: %f  max: %f  avg: %f' %
         #              (np.min(self._features),
         #               np.max(self._features),
@@ -289,37 +298,49 @@ class Datasplit(Dataset):
         if self.remove_background:
             task_indices = set(task_indices) - set(self.corpus._background_indices)
         task_indices = sorted(task_indices)
-        gt_single = [gt_t[0] for gt_t in video_obj.gt()]
+        if video_obj.has_label:
+            gt_single = [gt_t[0] for gt_t in video_obj.gt()]
 
         if self.subsample != 1:
-            subsample_indices = np.arange(len(gt_single) // self.subsample) * self.subsample
-            subsample_boundaries = list(zip(list(subsample_indices), list(subsample_indices-1)[1:] + [len(gt_single)-1]))
-            gt_single_sampled = list(np.array(gt_single)[subsample_indices])
+            subsample_indices = np.arange(features.shape[0] // self.subsample) * self.subsample
+            subsample_boundaries = list(
+                zip(list(subsample_indices), list(subsample_indices - 1)[1:] + [features.shape[0] - 1]))
+            if video_obj.has_label:
+                gt_single_sampled = list(np.array(gt_single)[subsample_indices])
             features = features[subsample_indices]
         else:
-            subsample_indices = np.arange(len(gt_single))
+            subsample_indices = np.arange(features.shape[0])
             subsample_boundaries = list(zip(subsample_indices, subsample_indices))
-            gt_single_sampled = gt_single
+            if video_obj.has_label:
+                gt_single_sampled = gt_single
 
         if wrap_torch:
             features = torch.from_numpy(features).float()
             task_indices = torch.LongTensor(task_indices)
-            gt_single = torch.LongTensor(gt_single)
-            gt_single_sampled = torch.LongTensor(gt_single_sampled)
+            if video_obj._has_label:
+                gt_single = torch.LongTensor(gt_single)
+                gt_single_sampled = torch.LongTensor(gt_single_sampled)
         else:
             task_indices = list(task_indices)
+
+        if self.feature_downscale != 1.0:
+            features = features / self.feature_downscale
         data = {
             'task_name': task_name,
             'video_name': video_name,
             'features': features,
-            'gt': video_obj.gt(),
-            'gt_single_unsampled': gt_single,
-            'gt_single': gt_single_sampled,
-            'gt_with_background': video_obj.gt_with_background(),
             'task_indices': task_indices,
             'subsample_indices': subsample_indices,
             'subsample_boundaries': subsample_boundaries,
         }
+
+        if video_obj._has_label:
+            data.update({
+                'gt': video_obj.gt(),
+                'gt_single_unsampled': gt_single,
+                'gt_single': gt_single_sampled,
+                'gt_with_background': video_obj.gt_with_background(),
+            })
         return data
 
     def _get_by_index(self, index, wrap_torch=True):
@@ -333,7 +354,8 @@ class Datasplit(Dataset):
     def _load_ground_truth_and_videos(self, remove_background):
         raise NotImplementedError("subclasses should implement _load_ground_truth")
 
-    def accuracy_corpus(self, optimal_assignment: bool, prediction_function, prefix='', verbose=True, compare_to_folder=None):
+    def accuracy_corpus(self, optimal_assignment: bool, prediction_function, prefix='', verbose=True,
+                        compare_to_folder=None):
         """Calculate metrics as well with previous correspondences between
         gt labels and output labels"""
         stats_by_task = {}
@@ -373,7 +395,6 @@ class Datasplit(Dataset):
                             key: np.array(val)
                             for key, val in pred_data.items()
                         }
-
 
             for video_name, video in self._videos_by_task[task].items():
                 # long_gt += list(video._gt_with_0)
@@ -492,6 +513,7 @@ class Datasplit(Dataset):
     #         video.resume()
     #     self._count_subact()
 
+
 class BatchSampler(Sampler):
     def __init__(self, datasplit: Datasplit, batch_size: int, batch_by_task: bool, shuffle: bool, seed=1):
         self.datasplit = datasplit
@@ -501,24 +523,25 @@ class BatchSampler(Sampler):
         self.seed = seed
 
         self.batches = []
+        if shuffle:
+            self.random_state = random.Random(self.seed)
+        else:
+            self.random_state = None
         task_names = list(sorted(self.datasplit._videos_by_task.keys()))
 
         videos_by_task = {
             task: list(sorted(videos))
             for task, videos in self.datasplit._videos_by_task.items()
         }
-        if self.shuffle:
-            state = random.Random(self.seed)
-            state.shuffle(task_names)
-            for videos in videos_by_task.values():
-                state.shuffle(videos)
 
         for task in task_names:
             videos = videos_by_task[task]
             for i in range(0, len(videos), self.batch_size):
-                self.batches.append([(task, video) for video in videos[i:i+self.batch_size]])
+                self.batches.append([(task, video) for video in videos[i:i + self.batch_size]])
 
     def __iter__(self):
+        if self.random_state is not None:
+            self.random_state.shuffle(self.batches)
         return iter(self.batches)
 
     def __len__(self):
@@ -540,18 +563,16 @@ class Corpus(object):
 
         self.label_indices2component_indices = {}
 
-        self._cache_features= cache_features
+        self._cache_features = cache_features
 
         self._labels_frozen = False
         self._background_labels = background_labels
         self._background_indices = []
         for label in background_labels:
             self._background_indices.append(self._index(label))
+        self._indices_by_task = {}
         self._load_mapping()
         self._labels_frozen = True
-
-        self._indices_by_task = {}
-
 
     @property
     def n_classes(self):
@@ -579,7 +600,8 @@ class Corpus(object):
 
     def _index_component(self, component_label):
         if component_label not in self.component2index:
-            assert not self._labels_frozen, "trying to index COMPONENT {} after index has been frozen".format(component_label)
+            assert not self._labels_frozen, "trying to index COMPONENT {} after index has been frozen".format(
+                component_label)
             component_idx = len(self.component2index)
             self.component2index[component_label] = component_idx
             self.index2component[component_idx] = component_label
@@ -684,5 +706,3 @@ class GroundTruth(object):
     #         sparse_segm = [i for i in val[::10]]
     #         self.gt[key] = sparse_segm
     #     self.gt_with_background = copy.deepcopy(self.gt)
-
-

src/models/framewise.py
@@ -134,7 +134,9 @@ class FramewiseDiscriminative(Model):
         assert use_labels
         loss = nn.CrossEntropyLoss()
         self.model.train()
-        optimizer = make_optimizer(self.args, self.model.parameters())
+        optimizer, scheduler = make_optimizer(self.args, self.model.parameters())
+        if scheduler is not None:
+            raise NotImplementedError("scheduler for discriminative model")
         loader = make_data_loader(self.args, train_data, batch_by_task=False, shuffle=True, batch_size=1)
 
         for epoch in range(self.args.epochs):

src/models/model.py
@@ -8,12 +8,30 @@ from utils.utils import all_equal
 
 def add_training_args(parser):
     parser.add_argument('--epochs', type=int, default=20)
+    parser.add_argument('--batch_accumulation', type=int, default=1)
     parser.add_argument('--lr', type=float, default=1e-3)
     parser.add_argument('--workers', type=int, default=0)
 
+    parser.add_argument('--reduce_plateau', action='store_true')
+    parser.add_argument('--reduce_plateau_factor', type=float, default=0.2)
+    parser.add_argument('--reduce_plateau_patience', type=float, default=1)
+    parser.add_argument('--reduce_plateau_min_lr', type=float, default=1e-4)
+
+
 
 def make_optimizer(args, parameters):
-    return torch.optim.Adam(parameters, lr=args.lr)
+    opt = torch.optim.Adam(parameters, lr=args.lr)
+    if args.reduce_plateau:
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+            opt, factor=args.reduce_plateau_factor,
+            verbose=True,
+            patience=args.reduce_plateau_patience,
+            min_lr=1e-4,
+            threshold=1e-5,
+        )
+    else:
+        scheduler = None
+    return opt, scheduler
 
 
 def padding_colate(data_samples):
@@ -33,7 +51,8 @@ def padding_colate(data_samples):
     data['lengths'] = torch.LongTensor(lengths)
 
     for key in pad_keys:
-        data[key] = torch.nn.utils.rnn.pad_sequence(unpacked[key], batch_first=True, padding_value=0)
+        if key in unpacked:
+            data[key] = torch.nn.utils.rnn.pad_sequence(unpacked[key], batch_first=True, padding_value=0)
 
     return data
 

src/models/semimarkov/semimarkov.py
@@ -19,6 +19,10 @@ class SemiMarkovModel(Model):
 
         parser.add_argument('--sm_component_model', action='store_true')
         parser.add_argument('--sm_component_decompose_steps', action='store_true')
+        parser.add_argument('--sm_component_mean_layers', type=int, default=1)
+        parser.add_argument('--sm_component_length_layers', type=int, default=1)
+        parser.add_argument('--sm_component_embedding_dim', type=int, default=100)
+        parser.add_argument('--sm_component_separate_embeddings', action='store_true')
 
 
     @classmethod
@@ -29,7 +33,7 @@ class SemiMarkovModel(Model):
         assert args.sm_max_span_length is not None
         if args.sm_component_model:
             if args.sm_component_decompose_steps:
-                assert not args.task_specific_steps, "can't decompose steps unless steps are across tasks; you should remove --task_specific_steps"
+                # assert not args.task_specific_steps, "can't decompose steps unless steps are across tasks; you should remove --task_specific_steps"
                 n_components = train_data.corpus.n_components
                 class_to_components = copy.copy(train_data.corpus.label_indices2component_indices)
             else:
@@ -42,10 +46,13 @@ class SemiMarkovModel(Model):
                                               n_components=n_components,
                                               class_to_components=class_to_components,
                                               feature_dim=feature_dim,
-                                              embedding_dim=100,
+                                              embedding_dim=args.sm_component_embedding_dim,
                                               allow_self_transitions=True,
                                               max_k=args.sm_max_span_length,
-                                              per_class_bias=True)
+                                              per_class_bias=True,
+                                              mean_layers=args.sm_component_mean_layers,
+                                              length_layers=args.sm_component_length_layers,
+                                              use_separate_embeddings=args.sm_component_separate_embeddings)
         else:
             model = SemiMarkovModule(n_classes,
                                      feature_dim,
@@ -81,7 +88,7 @@ class SemiMarkovModel(Model):
                 callback_fn(-1, {})
             else:
                 return
-        optimizer = make_optimizer(self.args, self.model.parameters())
+        optimizer, scheduler = make_optimizer(self.args, self.model.parameters())
         big_loader = make_data_loader(self.args, train_data, batch_by_task=False, shuffle=True, batch_size=100)
         samp = next(iter(big_loader))
         big_features = samp['features']
@@ -104,6 +111,7 @@ class SemiMarkovModel(Model):
 
         for epoch in range(self.args.epochs):
             losses = []
+            multi_batch_losses = []
             for batch in tqdm.tqdm(loader, ncols=80):
                 # if self.args.cuda:
                 #     features = features.cuda()
@@ -112,24 +120,19 @@ class SemiMarkovModel(Model):
                 tasks = batch['task_name']
                 videos = batch['video_name']
                 features = batch['features']
-                labels = batch['gt_single']
                 task_indices = batch['task_indices']
                 lengths = batch['lengths']
 
                 # assert len( task_indices) == self.n_classes, "remove_background and multi-task fit() not implemented"
 
-                # # add a batch dimension
-                # lengths = torch.LongTensor([features.size(0)]).unsqueeze(0)
-                # features = features.unsqueeze(0)
-                # labels = labels.unsqueeze(0)
-                # task_indices = task_indices.unsqueeze(0)
-
                 if self.args.cuda:
                     features = features.cuda()
                     lengths = lengths.cuda()
-                    labels = labels.cuda()
 
                 if use_labels:
+                    labels = batch['gt_single']
+                    if self.args.cuda:
+                        labels = labels.cuda()
                     spans = SemiMarkovModule.labels_to_spans(labels, max_k=K)
                 else:
                     spans = None
@@ -139,13 +142,19 @@ class SemiMarkovModel(Model):
                                                        valid_classes_per_instance=task_indices,
                                                        spans=spans,
                                                        add_eos=True)
-                this_loss.backward()
-
-                losses.append(this_loss.item())
-                # TODO: grad clipping?
-                optimizer.step()
-                self.model.zero_grad()
-            callback_fn(epoch, {'train_loss': np.mean(losses)})
+                multi_batch_losses.append(this_loss)
+                if len(multi_batch_losses) >= self.args.batch_accumulation:
+                    loss = sum(multi_batch_losses) / len(multi_batch_losses)
+                    loss.backward()
+                    multi_batch_losses = []
+                    losses.append(this_loss.item())
+                    # TODO: grad clipping?
+                    optimizer.step()
+                    self.model.zero_grad()
+            train_loss = np.mean(losses)
+            if scheduler is not None:
+                scheduler.step(train_loss)
+            callback_fn(epoch, {'train_loss': train_loss})
 
     def predict(self, test_data):
         self.model.eval()

src/models/semimarkov/semimarkov_modules.py
@@ -10,6 +10,8 @@ from torch_struct import SemiMarkovCRF
 
 from utils.utils import all_equal
 
+from torch.nn.init import xavier_uniform_
+
 BIG_NEG = -1e9
 
 
@@ -525,7 +527,10 @@ class ComponentSemiMarkovModule(SemiMarkovModule):
                  feature_dim: int,
                  embedding_dim: int,
                  allow_self_transitions=False, max_k=None,
-                 per_class_bias=False):
+                 per_class_bias=False,
+                 mean_layers=1,
+                 length_layers=1,
+                 use_separate_embeddings=False):
         self.n_components = n_components
         self.embedding_dim = embedding_dim
 
@@ -539,15 +544,27 @@ class ComponentSemiMarkovModule(SemiMarkovModule):
                     self.component_to_classes[component] = set()
                 self.component_to_classes[component].add(cls)
         self.per_class_bias = per_class_bias
+        self.mean_layers = mean_layers
+        self.length_layers = length_layers
+        self.use_separate_embeddings = use_separate_embeddings
         super(ComponentSemiMarkovModule, self).__init__(n_classes, feature_dim, allow_self_transitions, max_k=max_k)
 
     def init_params(self):
         # this initialization follows https://github.com/harvardnlp/compound-pcfg/blob/master/models.py
         # self.component_embeddings = nn.Parameter(torch.randn(self.n_components, self.embedding_dim))
-        self.component_embeddings = nn.EmbeddingBag(num_embeddings=self.n_components,
-                                                    embedding_dim=self.embedding_dim,
-                                                    mode="mean",
-                                                    sparse=False)
+        def make_embeddings():
+            return nn.EmbeddingBag(num_embeddings=self.n_components,
+                                   embedding_dim=self.embedding_dim,
+                                   mode="mean",
+                                   sparse=False)
+
+        if self.use_separate_embeddings:
+            self.initial_embeddings = make_embeddings()
+            self.transition_embeddings = make_embeddings()
+            self.emission_embeddings = make_embeddings()
+            self.length_embeddings = make_embeddings()
+        else:
+            self.shared_embeddings = make_embeddings()
 
         # p(class) \propto exp(w \cdot embed(class) + b_class)
         self.initial_weights = nn.Linear(self.embedding_dim, 1, bias=True)
@@ -563,16 +580,19 @@ class ComponentSemiMarkovModule(SemiMarkovModule):
         else:
             self.transition_bias = None
 
-        self.emission_mean_mlp = nn.Sequential(
-            ResidualLayer(self.embedding_dim, self.embedding_dim),
-            nn.Linear(self.embedding_dim, self.feature_dim)
-        )
+        emission_mean_mlp = [nn.Linear(self.embedding_dim, self.embedding_dim)]
+        for _ in range(self.mean_layers):
+            emission_mean_mlp.append(ResidualLayer(self.embedding_dim, self.embedding_dim))
+        emission_mean_mlp.append(nn.Linear(self.embedding_dim, self.feature_dim))
+        self.emission_mean_mlp = nn.Sequential(*emission_mean_mlp)
         self.emission_mean_bias = nn.Parameter(torch.zeros(self.feature_dim))
 
-        self.length_mlp = nn.Sequential(
-            ResidualLayer(self.embedding_dim, self.embedding_dim),
-            nn.Linear(self.embedding_dim, 1)
-        )
+        length_mlp = [nn.Linear(self.embedding_dim, self.embedding_dim)]
+        for _ in range(self.length_layers):
+            length_mlp.append(ResidualLayer(self.embedding_dim, self.embedding_dim))
+        length_mlp.append(nn.Linear(self.embedding_dim, 1))
+        self.length_mlp = nn.Sequential(*length_mlp)
+
         if self.per_class_bias:
             self.length_bias = nn.Parameter(torch.zeros(self.n_classes))
         else:
@@ -581,6 +601,10 @@ class ComponentSemiMarkovModule(SemiMarkovModule):
         # shared, tied, diagonal covariance matrix
         gaussian_cov = torch.eye(self.feature_dim).float()
         self.gaussian_cov = nn.Parameter(gaussian_cov, requires_grad=False)
+
+        for name, param in self.named_parameters():
+            if param.dim() > 1 and name not in ['gaussian_cov']:
+                xavier_uniform_(param)
 
     def fit_supervised(self, feature_list, label_list, state_smoothing=1e-2, length_smoothing=1e-1):
         raise NotImplementedError()
@@ -591,9 +615,9 @@ class ComponentSemiMarkovModule(SemiMarkovModule):
         self.emission_mean_bias.data.zero_()
         self.emission_mean_bias.data.add_(mean.squeeze(0))
 
-    def embed_classes(self, valid_classes):
+    def embed_classes(self, component_embeddings, valid_classes):
         if valid_classes is None:
-            valid_classes = torch.arange(self.n_classes, device=self.component_embeddings.weight.device)
+            valid_classes = torch.arange(self.n_classes, device=component_embeddings.weight.device)
         assert valid_classes.dim() == 1, valid_classes
         offset = 0
         offsets = [offset]
@@ -607,13 +631,16 @@ class ComponentSemiMarkovModule(SemiMarkovModule):
         offsets = offsets[:-1]
 
         # len(valid_classes) x embedding_dim
-        return self.component_embeddings(
-            torch.tensor(indices, device=self.component_embeddings.weight.device, dtype=torch.long),
-            torch.tensor(offsets, device=self.component_embeddings.weight.device, dtype=torch.long))
+        return component_embeddings(
+            torch.tensor(indices, device=component_embeddings.weight.device, dtype=torch.long),
+            torch.tensor(offsets, device=component_embeddings.weight.device, dtype=torch.long))
 
     def initial_log_probs(self, valid_classes):
         # len(valid_classes) x embedding_dim
-        class_embeddings = self.embed_classes(valid_classes)
+        class_embeddings = self.embed_classes(
+            self.initial_embeddings if self.use_separate_embeddings else self.shared_embeddings,
+            valid_classes
+        )
         # len(valid_clases) x 1
         x = self.initial_weights(class_embeddings)
         x = x.squeeze(1)
@@ -625,7 +652,10 @@ class ComponentSemiMarkovModule(SemiMarkovModule):
         # p(class_2 | class_1) \propto exp(f(embed(class_1)) embed(class_2) + b_class_2)
 
         # len(valid_classes) x embedding_dim
-        class_embeddings = self.embed_classes(valid_classes)
+        class_embeddings = self.embed_classes(
+            self.transition_embeddings if self.use_separate_embeddings else self.shared_embeddings,
+            valid_classes
+        )
         # len(valid_classes) x embedding_dim
         x = self.transition_weights(class_embeddings)
         x = torch.einsum("fe,te->tf", [x, class_embeddings])
@@ -640,7 +670,10 @@ class ComponentSemiMarkovModule(SemiMarkovModule):
 
     def emission_log_probs(self, features, valid_classes):
         # len(valid_classes) x embedding_dim
-        class_embeddings = self.embed_classes(valid_classes)
+        class_embeddings = self.embed_classes(
+            self.emission_embeddings if self.use_separate_embeddings else self.shared_embeddings,
+            valid_classes
+        )
         # len(valid_classes) x feature_dim
         class_means = self.emission_mean_mlp(class_embeddings)
         class_means += self.emission_mean_bias.unsqueeze(0).expand_as(class_means)
@@ -648,7 +681,10 @@ class ComponentSemiMarkovModule(SemiMarkovModule):
 
     def length_log_probs(self, valid_classes):
         # len(valid_classes) x embedding_dim
-        class_embeddings = self.embed_classes(valid_classes)
+        class_embeddings = self.embed_classes(
+            self.length_embeddings if self.use_separate_embeddings else self.shared_embeddings,
+            valid_classes
+        )
         # len(valid_classes)
         class_log_rates = self.length_mlp(class_embeddings).squeeze(1)
         if self.length_bias is not None: